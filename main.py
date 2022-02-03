624dc96155c658876309985d2af502bc77f502e1
iaraujo22 <araujocv22@gmail.com>
2022-02-02 22:27:16 -0500

    add functions fixing errors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Ivanildo Araujo (Ivan)
 # COMP 490-001
-import location as location
+import unittest.result
+
 import requests
 import secrets
 
@@ -10,9 +11,23 @@ def get_250_televisionShows():
     result = requests.get(location1)
     if result.status_code != 200:
         print("Help!")
+        return
     data = result.json()
     with open('data.txt', 'w') as f:
         f.write(data)
 
+
 def user_ratings():
-    location2 = f"https://imdb-api.com/api#UserRatings-header"
+    location2 = f"https://imdb-api.com/api#UserRatings-header/{secrets.secre_key}/tt7462410"
+    results = requests.get(location2)
+    if results.status_code != 200:
+        print("Help!")
+        return
+    data = results.json()
+    with open('data.txt', 'w') as f:
+        f.write(data)
+
+
+if __name__ == '__main__':
+    get_250_televisionShows()
+    user_ratings()
