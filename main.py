5f4e883eb6d6a2e4e27a5e31ce6920656ee40012
iaraujo22 <araujocv22@gmail.com>
2022-02-02 22:10:40 -0500

    add functions fixing errors

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # Ivanildo Araujo (Ivan)
-#COMP 490-001
-
-def get_250_televisionShows:
-
-
-
+# COMP 490-001
+import location as location
+import requests
+import secrets
 
 
+def get_250_televisionShows():
+    location1 = f"https://imdb-api.com/api/{secrets.secre_key}/tt7462410"
+    result = requests.get(location1)
+    if result.status_code != 200:
+        print("Help!")
+    data = result.json()
+    with open('data.txt', 'w') as f:
+        f.write(data)
 
-
-def main():
-
+def user_ratings():
+    location2 = f"https://imdb-api.com/api#UserRatings-header"
